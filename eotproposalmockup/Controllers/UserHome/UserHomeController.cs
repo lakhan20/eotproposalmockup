957ec1f468c4c166f9e16b3539aaa611ad6e5960
lakhan20 <lakhanrthakkar22@gmail.com>
2024-07-26 18:14:46 +0530

    Add project files.

---
+++ eotproposalmockup/Controllers/UserHome/UserHomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eotproposalmockup.Controllers.UserHome
+{
+    public class UserHomeController : Controller
+    {
+        // GET: UserHome
+        public ActionResult UserHomeView()
+        {
+            
+            return View();
+        }
+    }
+}
