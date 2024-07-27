342a672dc1204ed0d571c4456e90b239c7242f53
lakhanthakkar2002 <22004401110116@ljku.edu.in>
2024-07-27 19:13:36 +0530

    whose default validation and api calling done

---
+++ eotproposalmockup/Controllers/Eot/ClaimingWhoseDefaultController.cs
@@ -0,0 +1,72 @@
+﻿using eotproposalmockup.Models;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eotproposalmockup.Controllers.Eot
+{
+    public class ClaimingWhoseDefaultController : Controller
+    {
+        // GET: ClaimingWhoseDefault
+
+        NpgsqlConnection _connection;
+        public ClaimingWhoseDefaultController()
+        {
+            _connection = new NpgsqlConnection();
+            _connection.ConnectionString = ConfigurationManager.ConnectionStrings["MyconnectionString"].ToString();
+
+        }
+
+        [HttpGet]
+        public ActionResult getAllWhoseDefault()
+        {
+            List<claimingwhosedefault> whosedefaultlst = new List<claimingwhosedefault>();
+
+            try
+            {
+                _connection.Open();
+                string getAllClaimingWhoseDefaultSQL = "SELECT * from public.fn_getallclaimingwhosedefault()";
+                using(var command=new NpgsqlCommand(getAllClaimingWhoseDefaultSQL,_connection))
+                {
+                    using(var reader = command.ExecuteReader())
+                    {
+                        while(reader.Read()) {
+                            claimingwhosedefault whosedefault = new claimingwhosedefault();
+                            whosedefault.whosedefault_id = reader.GetInt32(0);
+                            whosedefault.whosedefault_description = reader.GetString(1);
+                            whosedefaultlst.Add(whosedefault);
+
+                        }
+
+                    }
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                return Json(new { status = false, message = "Something went wrong" },JsonRequestBehavior.AllowGet);
+            }
+            if (whosedefaultlst.Count > 0)
+            {
+                return Json(new { status = true, message = "Data fetched successfully..", whosedefaultlst },JsonRequestBehavior.AllowGet);
+
+
+            }
+            else
+            {
+                return Json(new { status =false, message = "No Data Found"}, JsonRequestBehavior.AllowGet);
+
+
+            }
+
+            
+        }
+
+      
+    }
+}
