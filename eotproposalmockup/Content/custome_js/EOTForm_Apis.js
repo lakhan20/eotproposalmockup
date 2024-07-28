a3ce1abf632ed25623e7c66276c497170194dd73
lakhanthakkar2002 <22004401110116@ljku.edu.in>
2024-07-28 22:14:31 +0530

    all validation applied

---
+++ eotproposalmockup/Content/custome_js/EOTForm_Apis.js
@@ -0,0 +1,24 @@
+﻿$(document).ready(function () {
+
+    var getroleId = localStorage.getItem("role_id");
+
+    if (getroleId == null || getroleId != 2) {
+        window.location.href = "/";
+    }
+    $.get("/ClaimingWhoseDefault/getAllWhoseDefault", (data) => {
+        if (data.status) {
+            const selectElement = $('#whoseDefault');
+            data.whosedefaultlst.forEach(item => {
+                const option = $('<option>', {
+                    id: item.id, 
+                    value: item.whosedefault_id, 
+                    text: item.whosedefault_description 
+                });
+                selectElement.append(option);
+            });
+        } else {
+            console.error("Error fetching data");
+        }
+    });
+
+})
